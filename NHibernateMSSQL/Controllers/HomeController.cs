9e7e6dcc90c3537887f781fe4e21e523d2c50e4b
Syrob <pawel13bor@gmail.com>
2017-06-08 15:31:57 +0200

    NHibernate działa

---
+++ NHibernateMSSQL/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using NHibernate;
+using NHibernate.Linq;
+using NHibernateMSSQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NHibernateMSSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			using (ISession session = NHibernateSession.OpenSession())
+			{
+				var albums = session.Query<Artist>().FirstOrDefault().Albums.ToList();
+				return View(albums);
+			}
+        }
+    }
+}
---
+++ NHibernateMSSQL/Models/NHibernateSession.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHibernateMSSQL.Models
+{
+	public static class NHibernateSession
+	{
+		public static ISession OpenSession()
+		{
+			var configuration = new Configuration();
+			var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\hibernate.cfg.xml");
+			configuration.Configure(configurationPath);
+
+			var ArtistConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Artist.hbm.xml");
+			configuration.AddFile(ArtistConfigurationFile);
+			var AlbumConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Album.hbm.xml");
+			configuration.AddFile(AlbumConfigurationFile);
+
+
+			ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+			return sessionFactory.OpenSession();
+		}
+	}
+}
